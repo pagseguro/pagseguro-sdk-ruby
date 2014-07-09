97db52b8ea96d80005bfb57d6d0e6c6308d0405d
Rodrigo DeAlmeida <rodrigoddalmeida@gmail.com>
2014-07-09 17:32:43 -0300

    delegate config information to a threadsafe object

---
+++ spec/pagseguro/config_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe PagSeguro::Config do
+  it_behaves_like "a configuration"
+end
---
+++ spec/support/shared_examples_for_configuration.rb
@@ -0,0 +1,8 @@
+RSpec.shared_examples "a configuration" do
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:email=) }
+  it { is_expected.to respond_to(:receiver_email) }
+  it { is_expected.to respond_to(:receiver_email=) }
+  it { is_expected.to respond_to(:token) }
+  it { is_expected.to respond_to(:token=) } 
+end
