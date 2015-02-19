98a291ed1474e38c2d6e8a7e4c1642cce3e097a7
Thiago Xavier <thiagoxvo@gmail.com>
2015-02-19 18:47:46 -0200

    Fix url path

--- spec/pagseguro/pre_approval_request_spec.rb
+++ spec/pagseguro/pre_approval_request_spec.rb
@@ -55,7 +55,7 @@ describe PagSeguro::PreApprovalRequest do
 
       PagSeguro::Request
         .should_receive(:post)
-        .with("pre_approvals", params)
+        .with("pre-approvals/request", params)
 
       pre_approval.register
     end
