07683fba2dc3b1a468d9a4e8e3552750cb6ce9a8
lucas renan <contato@lucasrenan.com>
2015-03-04 18:17:55 -0300

    Adds a find method for installment

--- lib/pagseguro/installment.rb
+++ lib/pagseguro/installment.rb
@@ -7,5 +7,19 @@ module PagSeguro
 
     # Set the credit card brand
     attr_accessor :credit_card_brand
+
+    # Find installment options by a given amount
+    def self.find(amount)
+      load_from_response Request.get("installments?amount=#{amount}")
+    end
+
+    # Serialize the HTTP response into data.
+    def self.load_from_response(response) # :nodoc:
+      if response.success? and response.xml?
+        "success"
+      else
+        Response.new Errors.new(response)
+      end
+    end
   end
 end
