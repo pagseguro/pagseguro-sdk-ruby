bcf254d56a34da4d1c9a54c3b5632bb22f040bb9
lucas renan <contato@lucasrenan.com>
2015-03-04 18:17:55 -0300

    Adds a find method for installment

---
+++ lib/pagseguro/installment/response.rb
@@ -0,0 +1,12 @@
+module PagSeguro
+  class Installment
+    class Response
+      # Set the response errors.
+      attr_reader :errors
+
+      def initialize(errors = Errors.new)
+        @errors = errors
+      end
+    end
+  end
+end
--- spec/pagseguro/installment_spec.rb
+++ spec/pagseguro/installment_spec.rb
@@ -2,4 +2,33 @@ require "spec_helper"
 
 describe PagSeguro::Installment do
   it_assigns_attribute :credit_card_brand
+
+  describe ".find" do
+    context "when request succeeds" do
+      let(:request) { double("request") }
+
+      it "finds installments by the given amount" do
+        expect(PagSeguro::Request).to receive(:get)
+          .with("installments?amount=100")
+          .and_return(request)
+        expect(PagSeguro::Installment).to receive(:load_from_response)
+          .with(request)
+
+        PagSeguro::Installment.find(100)
+      end
+    end
+
+    context "when request fails" do
+      it "returns response with errors" do
+        body = %[<?xml version="1.0"?><errors><error><code>1234</code>
+          <message>Sample error</message></error></errors>]
+        FakeWeb.register_uri :get, %r[.+], status: [400, "Bad Request"],
+          body: body, content_type: "text/xml"
+        response = PagSeguro::Installment.find("invalid")
+
+        expect(response).to be_a(PagSeguro::Installment::Response)
+        expect(response.errors).to include("Sample error")
+      end
+    end
+  end
 end
