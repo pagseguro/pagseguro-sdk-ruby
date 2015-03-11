ed87752ae85719c12f606239fa46ab62848ab13c
lucas renan <contato@lucasrenan.com>
2015-03-11 16:59:24 -0300

    Adds a load_from_response method to session

--- lib/pagseguro/session.rb
+++ lib/pagseguro/session.rb
@@ -5,8 +5,19 @@ module PagSeguro
     # The session id.
     attr_accessor :id
 
+    # Create a payment session.
+    # Return a PagSeguro::Session instance.
     def self.create
-      Request.post("sessions")
+      load_from_response Request.post("sessions")
+    end
+
+    # Serialize the HTTP response into data.
+    def self.load_from_response(response) # :nodoc:
+      if response.success? and response.xml?
+        Nokogiri::XML(response.body).css("session").first
+      else
+        "error: #{response.inspect}"
+      end
     end
   end
 end
