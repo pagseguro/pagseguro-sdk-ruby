922948bafe42bd7228e2ea4aac538fb24abb261d
Vitor Kusiaki <vkusiaki@gmail.com>
2015-03-27 19:17:56 -0300

    Moves most logic to a proper class

--- lib/pagseguro/authorization.rb
+++ lib/pagseguro/authorization.rb
@@ -1,22 +1,7 @@
 module PagSeguro
   class Authorization
-    PERMISSIONS_SYMBOLS = %i(checkouts notifications searches pre_approvals payments)
-    PERMISSIONS = {
-      checkouts: 'CREATE_CHECKOUTS',
-      notifications: 'RECEIVE_TRANSACTION_NOTIFICATIONS',
-      searches: 'SEARCH_TRANSACTIONS',
-      pre_approvals: 'MANAGE_PAYMENT_PRE_APPROVALS',
-      payments: 'DIRECT_PAYMENTS'
-    }
-
-    def self.authorize(credentials, permissions = PERMISSIONS_SYMBOLS)
-      permissions = set_permissions(permissions)
-      Request.post('/authorizations/request', 'v2', permissions)
-    end
-
-    private
-    def self.set_permissions(permissions)
-      permissions.map { |value| PERMISSIONS[value] }.join(',')
+    def self.authorize(credentials, permissions = nil)
+      Request.post('/authorizations/request', 'v2')
     end
   end
 end
