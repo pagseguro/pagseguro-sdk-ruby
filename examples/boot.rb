9b678f4d783b139531a2c9e1c5f74d6ce6c54097
lucas renan <contato@lucasrenan.com>
2015-03-27 19:05:11 -0300

    Adds installment to transaction request

--- examples/boot.rb
+++ examples/boot.rb
@@ -3,9 +3,7 @@ require "pagseguro"
 
 I18n.locale = "pt-BR"
 
-PagSeguro.environment = :sandbox
-
 PagSeguro.configure do |config|
-  config.token = ENV.fetch("PAGSEGURO_TOKEN") { "FBA1D3AED5DC476185082686E21B1CB8" }
-  config.email = ENV.fetch("PAGSEGURO_EMAIL") { "contato@lucasrenan.com" }
+  config.token = ENV.fetch("PAGSEGURO_TOKEN")
+  config.email = ENV.fetch("PAGSEGURO_EMAIL")
 end
