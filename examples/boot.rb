849da23a99e2047fe68fc171d9d67461dc4a3a78
Vitor Kusiaki <vkusiaki@gmail.com>
2015-03-31 17:52:33 -0300

    Adds an example of how authorization app works, using app_id and app_key

--- examples/boot.rb
+++ examples/boot.rb
@@ -6,5 +6,7 @@ I18n.locale = "pt-BR"
 PagSeguro.configure do |config|
   config.token = ENV.fetch("PAGSEGURO_TOKEN")
   config.email = ENV.fetch("PAGSEGURO_EMAIL")
+  config.app_id = ENV.fetch("PAGSEGURO_APP_ID")
+  config.app_key = ENV.fetch("PAGSEGURO_APP_KEY")
   config.environment = :sandbox
 end
