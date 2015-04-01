b42e2c8b2d11f6a17bfd72aa8cba682769789c1f
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-01 14:31:10 -0300

    Moves permissions constant to authorization class

--- spec/pagseguro/authorization/serializer_spec.rb
+++ spec/pagseguro/authorization/serializer_spec.rb
@@ -10,11 +10,4 @@ describe PagSeguro::Authorization::Serializer do
   it{ expect(params).to include(permissions: 'CREATE_CHECKOUTS,RECEIVE_TRANSACTION_NOTIFICATIONS') }
   it{ expect(params).to include(notificationURL: 'www.foo.com.br') }
   it{ expect(params).to include(redirectURL: 'www.bar.io') }
-
-  context 'when the permissions are default' do
-    it 'gives all permissions' do
-      expect(described_class.new(notification_url, redirect_url).to_params)
-        .to include(permissions: 'CREATE_CHECKOUTS,RECEIVE_TRANSACTION_NOTIFICATIONS,SEARCH_TRANSACTIONS,MANAGE_PAYMENT_PRE_APPROVALS,DIRECT_PAYMENTS')
-    end
-  end
 end
