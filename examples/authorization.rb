7945ac49d4620a3d8bba45479a4664718729a0cf
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-02 16:45:37 -0300

    Updates authorization example to pass an hash with params

--- examples/authorization.rb
+++ examples/authorization.rb
@@ -1,7 +1,15 @@
 require_relative "boot"
 
-permissions = [:searches, :notifications]
-response = PagSeguro::Authorization.authorize('foo.com.br', 'bar.com.br', permissions)
+# app_id defaults to PagSeguro.app_id
+# app_key defaults to PagSeguro.app_key
+# permissions defaults to all permissions
+
+options = {
+  app_id: ENV.fetch("PAGSEGURO_APP_ID"),
+  app_key: ENV.fetch("PAGSEGURO_APP_KEY"),
+  permissions: [:searches, :notifications]
+}
+response = PagSeguro::Authorization.authorize(options,'foo.com.br', 'bar.com.br')
 
 puts "=> Response"
 puts response.code
