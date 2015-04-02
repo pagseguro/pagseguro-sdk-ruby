1d25a66fff58f42f608f70f81237104de799d818
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-02 18:28:18 -0300

    Updates authorization example to wrap all params in an object

--- examples/authorization.rb
+++ examples/authorization.rb
@@ -3,13 +3,17 @@ require_relative "boot"
 # app_id defaults to PagSeguro.app_id
 # app_key defaults to PagSeguro.app_key
 # permissions defaults to all permissions
+# notification_url is a required param
+# redirect_url is a required param
 
 options = {
   app_id: ENV.fetch("PAGSEGURO_APP_ID"),
   app_key: ENV.fetch("PAGSEGURO_APP_KEY"),
-  permissions: [:searches, :notifications]
+  permissions: [:searches, :notifications],
+  notification_url: 'foo.com.br',
+  redirect_url: 'bar.com.br'
 }
-response = PagSeguro::Authorization.authorize(options,'foo.com.br', 'bar.com.br')
+response = PagSeguro::Authorization.new(options).authorize
 
 puts "=> Response"
 puts response.code
