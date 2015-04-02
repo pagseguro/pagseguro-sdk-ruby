0331cb4c6ee3b9f9058bb730a5f55d690ed5bb8b
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-02 16:28:44 -0300

    Updates authorization test with new params

--- spec/pagseguro/authorization_spec.rb
+++ spec/pagseguro/authorization_spec.rb
@@ -4,6 +4,8 @@ describe PagSeguro::Authorization do
   describe '.authorize' do
     it 'makes a successful authorization' do
       params = {
+        appId: 'app123',
+        appKey: 'adsada',
         notificationURL: "foo",
         redirectURL: "bar",
         permissions: "RECEIVE_TRANSACTION_NOTIFICATIONS,SEARCH_TRANSACTIONS"
