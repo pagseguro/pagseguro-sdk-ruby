6ea361c91c21ab83d88e8fc6356080250cff9b69
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-02 15:25:38 -0300

    Makes an authorization object to pass to request

--- lib/pagseguro/authorization.rb
+++ lib/pagseguro/authorization.rb
@@ -1,5 +1,20 @@
 module PagSeguro
   class Authorization
+    include Extensions::MassAssignment
+
+    # The application id
+    attr_accessor :app_id
+
+    # The token related to the application
+    attr_accessor :app_key
+
+    # The permissions given to the application
+    # Defaults to all permissions
+    attr_accessor :permissions
+
+    # The seller reference (optional)
+    attr_accessor :reference
+
     PERMISSIONS = {
       checkouts: 'CREATE_CHECKOUTS',
       notifications: 'RECEIVE_TRANSACTION_NOTIFICATIONS',
@@ -8,9 +23,17 @@ module PagSeguro
       payments: 'DIRECT_PAYMENTS'
     }
 
-    def self.authorize(notification_url, redirect_url, permissions = PERMISSIONS)
-      params = Serializer.new(notification_url, redirect_url, permissions).to_params
+    def self.authorize(options, notification_url, redirect_url)
+      authorization = new(options)
+      params = Serializer.new(authorization, notification_url, redirect_url).to_params
       Response.new Request.post('/authorizations/request', params)
+    end
+
+    private
+    def before_initialize
+      self.app_id = PagSeguro.app_id
+      self.app_key = PagSeguro.app_key
+      self.permissions = PERMISSIONS.keys
     end
   end
 end
