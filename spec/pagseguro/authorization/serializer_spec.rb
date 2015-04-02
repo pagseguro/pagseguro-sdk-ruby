7c7bda5fa69ed1b2a9cc03b9199a012a69126e53
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-02 15:31:02 -0300

    Receives an authorization object to serialize params

--- spec/pagseguro/authorization/serializer_spec.rb
+++ spec/pagseguro/authorization/serializer_spec.rb
@@ -1,13 +1,18 @@
 require "spec_helper"
 
 describe PagSeguro::Authorization::Serializer do
-  let(:notification_url) { 'www.foo.com.br' }
-  let(:redirect_url) { 'www.bar.io' }
-  let(:permissions) { [:checkouts, :notifications] }
-  let(:serializer) { described_class.new(notification_url, redirect_url, permissions) }
+  let(:options) do
+    {
+      app_id: 'app12345',
+      app_key: 'KAdjfeAI3',
+      permissions: [:checkouts, :notifications]
+    }
+  end
+  let(:authorization) { PagSeguro::Authorization.new(options) }
+  let(:serializer) { described_class.new(authorization, 'foo.com', 'bar.com') }
   let(:params) { serializer.to_params }
 
   it{ expect(params).to include(permissions: 'CREATE_CHECKOUTS,RECEIVE_TRANSACTION_NOTIFICATIONS') }
-  it{ expect(params).to include(notificationURL: 'www.foo.com.br') }
-  it{ expect(params).to include(redirectURL: 'www.bar.io') }
+  it{ expect(params).to include(notificationURL: 'foo.com') }
+  it{ expect(params).to include(redirectURL: 'bar.com') }
 end
