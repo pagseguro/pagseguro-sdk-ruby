ae533e39ad908d5f3badfce2ea6fef2fc5bc3cb9
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-08 13:54:48 -0300

    Updates credential example and docs

--- lib/pagseguro/extensions/credentiable.rb
+++ lib/pagseguro/extensions/credentiable.rb
@@ -3,8 +3,10 @@ module PagSeguro
     module Credentiable
       include Extensions::EnsureType
 
+      # Get the credential object value
       attr_reader :credentials
 
+      # Set a credential object
       def credentials=(credentials)
         @credentials = ensure_type(Credentials, credentials)
       end
