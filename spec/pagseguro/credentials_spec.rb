3e6b1cec47cad014e62c704b3292fed95a375faf
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-08 09:57:38 -0300

    Adds and optional authorization code to be used in payment request

--- spec/pagseguro/credentials_spec.rb
+++ spec/pagseguro/credentials_spec.rb
@@ -6,5 +6,6 @@ describe PagSeguro::Credentials do
 
     it { expect(credentials.app_id).to eq('app123') }
     it { expect(credentials.app_key).to eq('qwerty') }
+    it { expect(credentials.authorization_code).to be_nil }
   end
 end
