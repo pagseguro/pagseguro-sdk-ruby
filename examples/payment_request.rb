08edf13804663e2619f0395760af5abec7873c01
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-08 09:57:38 -0300

    Adds and optional authorization code to be used in payment request

--- examples/payment_request.rb
+++ examples/payment_request.rb
@@ -9,7 +9,7 @@ payment.redirect_url = "http://dev.simplesideias.com.br/?redirect"
 # You can pass a credentials object to be used in your
 # payment request, if you don't want to use email/token config.
 #
-# credentials = PagSeguro::Credentials.new("app123", "qwerty")
+# credentials = PagSeguro::Credentials.new("app123", "qwerty", "authorizationcode")
 # payment.credentials = credentials
 
 payment.items << {
