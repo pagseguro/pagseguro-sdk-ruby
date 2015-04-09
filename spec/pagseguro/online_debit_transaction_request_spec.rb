9b3db0b564c8a7bcd65888382033529cb2f8dad1
lucas renan <contato@lucasrenan.com>
2015-04-09 12:40:56 -0300

    Adds a required #payment_method method to especialized transaction request classes

--- spec/pagseguro/online_debit_transaction_request_spec.rb
+++ spec/pagseguro/online_debit_transaction_request_spec.rb
@@ -3,6 +3,12 @@ require "spec_helper"
 describe PagSeguro::OnlineDebitTransactionRequest do
   it_ensures_type PagSeguro::Bank, :bank
 
+  describe "#payment_method" do
+    it "is online_debit" do
+      expect(subject.payment_method).to eq("online_debit")
+    end
+  end
+
   it "sets the bank" do
     bank = PagSeguro::Bank.new
     payment = PagSeguro::OnlineDebitTransactionRequest.new(bank: bank)
