be438c0caaff2d8c8e3e2474008248e211a5404d
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-15 14:21:17 -0300

    Updates authorization tests by removing app_id and app_key attributes

--- spec/pagseguro/authorization_spec.rb
+++ spec/pagseguro/authorization_spec.rb
@@ -4,8 +4,6 @@ describe PagSeguro::Authorization do
   describe '.authorize' do
     it 'makes a successful authorization' do
       params = {
-        appId: 'app123',
-        appKey: 'adsada',
         notificationURL: "foo",
         redirectURL: "bar",
         permissions: "RECEIVE_TRANSACTION_NOTIFICATIONS,SEARCH_TRANSACTIONS"
@@ -18,8 +16,6 @@ describe PagSeguro::Authorization do
 
       PagSeguro::Authorization.new(
           {
-            app_id: 'app123',
-            app_key: 'adsada',
             permissions: [:notifications, :searches],
             notification_url: 'foo',
             redirect_url: 'bar'
