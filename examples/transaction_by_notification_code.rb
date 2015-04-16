c7602106ec5c0e47710acff011b0a4984b41c2fa
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-16 14:26:54 -0300

    Allows transaction search using application credentials

--- examples/transaction_by_notification_code.rb
+++ examples/transaction_by_notification_code.rb
@@ -5,7 +5,6 @@ require_relative "boot"
 # credentials = PagSeguro.account_credentials
 # or
 # credentials = PagSeguro::AccountCredentials.new("user@example.com", "token")
-
 # transaction = PagSeguro::Transaction.find_by_notification_code("3D939", { credentials: credentials })
 
 transaction = PagSeguro::Transaction.find_by_notification_code("3D939")
