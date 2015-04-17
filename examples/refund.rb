585d70ae1edb3b6e2777cc67099d4e5144e3d709
lucas renan <contato@lucasrenan.com>
2015-04-17 17:39:05 -0300

    Partial Refund

--- examples/refund.rb
+++ examples/refund.rb
@@ -3,6 +3,9 @@ require_relative "boot"
 refund = PagSeguro::Refund.new
 refund.transaction_code = "D5D5BE444148407891E497B421975599"
 
+# Partial refund
+# refund.value = 400.00
+
 response = refund.register
 
 if response.errors.any?
