46ad744d978f1f438c36f416d50bc9d811993148
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-20 15:07:51 -0300

    Updates examples with new credentials

--- examples/authorization_by_code.rb
+++ examples/authorization_by_code.rb
@@ -1,13 +1,20 @@
 require_relative 'boot'
 
+# credentials = PagSeguro::ApplicationCredentials.new("app452", "1D473")
+credentials = PagSeguro::AccountCredentials.new("app452", "1D473")
+
 options = {
-  credentials: PagSeguro::Credentials.new("app452", "1D473")
+  credentials: credentials
 }
 
 authorization = PagSeguro::Authorization.find_by_code('92FDQ3', options)
 
-authorization.permissions.each do |permission|
-  puts "Permission: "
-  puts permission.code
-  puts permission.status
+if authorization.errors.any?
+  puts authorization.errors.join("\n")
+else
+  authorization.permissions.each do |permission|
+    puts "Permission: "
+    puts permission.code
+    puts permission.status
+  end
 end
