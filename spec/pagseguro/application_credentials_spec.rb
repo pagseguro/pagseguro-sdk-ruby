2d641fee5765aeacda01984db24cc1892ad5c236
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-20 15:05:32 -0300

    Specializes credentials classes in account and application

--- spec/pagseguro/application_credentials_spec.rb
+++ spec/pagseguro/application_credentials_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe PagSeguro::Credentials do
+describe PagSeguro::ApplicationCredentials do
   describe 'credentials attributes' do
-    credentials = PagSeguro::Credentials.new('app123', 'qwerty')
+    credentials = PagSeguro::ApplicationCredentials.new('app123', 'qwerty')
 
     it { expect(credentials.app_id).to eq('app123') }
     it { expect(credentials.app_key).to eq('qwerty') }
