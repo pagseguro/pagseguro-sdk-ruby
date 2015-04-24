efce2f0638d2b6fe0e6daa6f1469191e9fd60a3c
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-24 14:42:06 -0300

    Updates authorization examples

--- examples/authorization_by_code.rb
+++ examples/authorization_by_code.rb
@@ -1,20 +1,20 @@
 require_relative 'boot'
 
 # credentials = PagSeguro::ApplicationCredentials.new("app452", "1D473")
-credentials = PagSeguro::AccountCredentials.new("app452", "1D473")
+credentials = PagSeguro::ApplicationCredentials.new("app45", "1D473")
 
 options = {
   credentials: credentials
 }
 
-authorization = PagSeguro::Authorization.find_by_code('92FDQ3', options)
+authorization = PagSeguro::Authorization.find_by_code('D9EED', options)
 
 if authorization.errors.any?
   puts authorization.errors.join("\n")
 else
   authorization.permissions.each do |permission|
     puts "Permission: "
-    puts permission.code
-    puts permission.status
+    puts "  code: #{permission.code}"
+    puts "  status: #{permission.status}"
   end
 end
