31cb1bb581a5474e7321b8a5070472dc86181775
Vitor Kusiaki <vkusiaki@gmail.com>
2015-04-29 12:20:23 -0300

    Updates examples to show how to use credentials object

--- examples/transaction_by_notification_code.rb
+++ examples/transaction_by_notification_code.rb
@@ -1,6 +1,11 @@
 require_relative "boot"
 
-# credentials = PagSeguro::Credentials.new("app45", "1D4738")
+# You have both options to instantiate account credentials:
+# The first option above defaults to PagSeguro.app_id and PagSeguro.app_key config
+# credentials = PagSeguro.account_credentials
+# or
+# credentials = PagSeguro::AccountCredentials.new("user@example.com", "token")
+
 # transaction = PagSeguro::Transaction.find_by_notification_code("3D939", { credentials: credentials })
 
 transaction = PagSeguro::Transaction.find_by_notification_code("3D939")
