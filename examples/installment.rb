6aa74bcb9b1d32123775fd31afdf89c0a54e2b29
Vitor Kusiaki <vkusiaki@gmail.com>
2015-05-04 17:45:23 -0300

    Permits passing credentials to installments search

--- examples/installment.rb
+++ examples/installment.rb
@@ -1,8 +1,5 @@
 require_relative "boot"
 
-credentials = PagSeguro.application_credentials
-credentials.authorization_code = "BF0C85F19BEB4011AC4D99C88C6E0638"
-
 installments = PagSeguro::Installment.find("100.00", {credentials: credentials})
 
 puts "=> INSTALLMENTS"
@@ -11,7 +8,8 @@ installments.each do |installment|
   puts installment.inspect
 end
 
-visa_installments = PagSeguro::Installment.find("100.00", { card_brand: "visa", credentials: credentials })
+
+visa_installments = PagSeguro::Installment.find("100.00", :visa)
 
 puts
 puts "=> VISA INSTALLMENTS"
