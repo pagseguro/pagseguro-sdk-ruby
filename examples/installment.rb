4d2d5f1922222a840727a2c344781cd763c4414b
cegoncalves <cegoncalves@uolinc.com>
2015-05-05 16:55:11 -0300

    Updates installments example

--- examples/installment.rb
+++ examples/installment.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
-installments = PagSeguro::Installment.find("100.00")
+credentials = PagSeguro.application_credentials
+credentials.authorization_code = "BF0C85F19BEB4011AC4D99C88C6E0638"
+
+installments = PagSeguro::Installment.find("100.00", {credentials: credentials})
 
 puts "=> INSTALLMENTS"
 puts
@@ -8,10 +11,7 @@ installments.each do |installment|
   puts installment.inspect
 end
 
-# credentials = PagSeguro::ApplicationCredentials.new("app123", 'token')
-visa_installments = PagSeguro::Installment.find("100.00", { card_brand: "visa" })
-# visa_installments = PagSeguro::Installment.find("100.00", { card_brand: "visa",
-#   credentials: credentials })
+visa_installments = PagSeguro::Installment.find("100.00", { card_brand: "visa", credentials: credentials })
 
 puts
 puts "=> VISA INSTALLMENTS"
