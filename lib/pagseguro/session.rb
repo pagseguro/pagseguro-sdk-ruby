9b745e0906f9fbbb6c4a10ee4b49ff4dab10b967
lucas renan <contato@lucasrenan.com>
2015-05-05 18:20:14 -0300

    Removes old methods for session response data parse

--- lib/pagseguro/session.rb
+++ lib/pagseguro/session.rb
@@ -1,28 +1,9 @@
 module PagSeguro
   class Session
-    include Extensions::MassAssignment
-
-    # The session id.
-    attr_accessor :id
-
     # Create a payment session.
-    # Return a PagSeguro::Session instance.
+    # Return a PagSeguro::Session::Response instance.
     def self.create
-      Response.new(Request.post("sessions", "v2"))
-    end
-
-    # Serialize the HTTP response into data.
-    def self.load_from_response(response) # :nodoc:
-      if response.success? and response.xml?
-        load_from_xml Nokogiri::XML(response.body).css("session").first
-      else
-        Response.new Errors.new(response)
-      end
-    end
-
-    # Serialize the XML object.
-    def self.load_from_xml(xml) # :nodoc:
-      new Serializer.new(xml).serialize
+      Response.new(Request.post("sessions", "v2")).parse
     end
   end
 end
