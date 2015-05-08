c939ec6fc9e0123196f7b96c8903d6aa6642921b
lucas renan <contato@lucasrenan.com>
2015-05-08 18:57:35 -0300

    Adds a parse method to session response

--- lib/pagseguro/session/response.rb
+++ lib/pagseguro/session/response.rb
@@ -17,12 +17,27 @@ module PagSeguro
       end
 
       def parse
-        # if response.success? and response.xml?
-        #   xml = Nokogiri::XML(response.body).css("session").first
-        #   Serializer.new(xml).serialize
-        # end
-
+        serialize_and_create_attributes!
         self
+      end
+
+      private
+      def serialize_and_create_attributes!
+        if response.success? and response.xml?
+          create_attributes(serialize)
+        end
+      end
+
+      def serialize
+        xml = Nokogiri::XML(response.body).css("session").first
+        Serializer.new(xml).serialize
+      end
+
+      def create_attributes(data)
+        data.each do |attr_name, attr_value|
+          self.class.send(:attr_reader, attr_name)
+          instance_variable_set("@#{attr_name}", attr_value)
+        end
       end
     end
   end
