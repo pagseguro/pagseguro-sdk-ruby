9fcb11552bc20b98c0ec120695969e7746eba69d
Vitor Kusiaki <vkusiaki@gmail.com>
2015-05-12 16:26:39 -0300

    Adds tests to search abandoned transactions

---
+++ spec/pagseguro/transaction/search/search_abandoned_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe PagSeguro::SearchAbandoned do
+  describe 'it searches abandoned transactions' do
+    let(:options) { { starts_at: Time.now, ends_at: Time.now, page: 1, per_page: 10 } }
+    let(:search) { PagSeguro::SearchAbandoned.new("transactions/abandoned", options, 1) }
+    let(:source) { File.read("./spec/fixtures/transactions/search.xml") }
+    let(:xml) { Nokogiri::XML(source) }
+    let(:response) { double(:response, data: xml, unauthorized?: false, bad_request?: false) }
+
+    describe 'the search abandoned' do
+      before do
+        FakeWeb.register_uri :get, %r[#{PagSeguro.api_url('v2/transactions/abandoned')}.*],
+          body: xml
+      end
+
+      let(:transaction) { double(:transaction) }
+
+      it 'returns an array of transactions' do
+        expect(PagSeguro::Transaction).to receive(:load_from_xml).exactly(2)
+          .times
+          .and_return(transaction)
+        expect(search.transactions).to eq([transaction, transaction])
+      end
+    end
+  end
+end
