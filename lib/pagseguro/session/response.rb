5d7ea743675a55fb6345448e7dd4e947f32c58bb
lucas renan <contato@lucasrenan.com>
2015-05-15 19:18:16 -0300

    Defines an api for session service

--- lib/pagseguro/session/response.rb
+++ lib/pagseguro/session/response.rb
@@ -1,44 +1,22 @@
 module PagSeguro
   class Session
     class Response
-      extend Forwardable
-
-      def_delegators :response, :success?
-
-      # The request response.
-      attr_reader :response
-
       def initialize(response)
         @response = response
       end
 
-      def errors
-        @errors ||= Errors.new(response)
-      end
-
-      def parse
-        serialize_and_create_attributes!
-        self
+      def serialize
+        if response.success? && response.xml?
+          xml = Nokogiri::XML(response.body).css("session").first
+          Serializer.new(xml).serialize
+        else
+          { errors: Errors.new(response) }
+        end
       end
 
       private
-      def serialize_and_create_attributes!
-        if response.success? and response.xml?
-          create_attributes(serialize)
-        end
-      end
-
-      def serialize
-        xml = Nokogiri::XML(response.body).css("session").first
-        Serializer.new(xml).serialize
-      end
-
-      def create_attributes(data)
-        data.each do |attr_name, attr_value|
-          self.class.send(:attr_reader, attr_name)
-          instance_variable_set("@#{attr_name}", attr_value)
-        end
-      end
+      # The request response.
+      attr_reader :response
     end
   end
 end
