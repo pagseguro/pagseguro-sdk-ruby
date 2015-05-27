7733a198c959fcc8704b5d2203417f43e72700c0
Vitor Kusiaki <vkusiaki@gmail.com>
2015-05-27 16:43:47 -0300

    Adds find methods to authorization

--- lib/pagseguro/authorization.rb
+++ lib/pagseguro/authorization.rb
@@ -18,13 +18,13 @@ module PagSeguro
     # Find an authorization by it's notification code
     def self.find_by_notification_code(code, options = {})
       request = Request.get("authorizations/notifications/#{code}", options)
-      new(Response.new(request).serialize)
+      new Response.new(request).serialize
     end
 
     # Find an authorization by it's code
     def self.find_by_code(code, options = {})
       request = Request.get("authorizations/#{code}", options)
-      new(Response.new(request).serialize)
+      new Response.new(request).serialize
     end
   end
 end
