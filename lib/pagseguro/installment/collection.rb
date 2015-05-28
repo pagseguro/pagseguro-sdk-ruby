4cd6c4d8d7c02bb649b1196d6c0802dc0aa33b60
lucas renan <contato@lucasrenan.com>
2015-05-28 18:23:16 -0300

    Installment .find method returns a Installment Collection instance

--- lib/pagseguro/installment/collection.rb
+++ lib/pagseguro/installment/collection.rb
@@ -1,7 +1,13 @@
 module PagSeguro
   class Installment
     class Collection
+      extend Forwardable
 
+      def_delegators :@installments, :each, :empty?, :any?, :join, :include?
+
+      def initialize(installments)
+        @installments = installments.map { |installment| Installment.new(installment) }
+      end
     end
   end
 end
