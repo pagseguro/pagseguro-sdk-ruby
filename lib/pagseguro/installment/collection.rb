83426d744b2a3937f98852c3717a3b244aaeaa88
lucas renan <contato@lucasrenan.com>
2015-05-29 20:22:07 -0300

    Adds missing specs to installment

--- lib/pagseguro/installment/collection.rb
+++ lib/pagseguro/installment/collection.rb
@@ -3,10 +3,23 @@ module PagSeguro
     class Collection
       extend Forwardable
 
-      def_delegators :@installments, :each, :empty?, :any?, :join, :include?
+      def_delegators :@installments, :each, :empty?, :any?
 
-      def initialize(installments)
-        @installments = installments.map { |installment| Installment.new(installment) }
+      def initialize(options)
+        @errors = options[:errors] if options[:errors]
+        @installments = instantiate_installments(options[:installments])
+      end
+
+      def errors
+        @errors ||= Errors.new
+      end
+
+      private
+      def instantiate_installments(installments)
+        return [] unless installments
+        installments.map do |installment|
+          Installment.new(installment)
+        end
       end
     end
   end
