5c81347e0022b762e4fad8b838156eeb06ae6aae
Vitor Kusiaki <vkusiaki@gmail.com>
2015-06-12 13:27:37 -0300

    Adds authorization collection class

---
+++ lib/pagseguro/authorization/collection.rb
@@ -0,0 +1,26 @@
+module PagSeguro
+  class Authorization
+    class Collection
+      extend Forwardable
+
+      def_delegators :@authorizations, :each, :empty?, :any?
+
+      def initialize(options)
+        @errors = options[:errors] if options[:errors]
+        @authorizations = instantiate_authorizations(options[:authorizations])
+      end
+
+      def errors
+        @errors ||= Errors.new
+      end
+
+      private
+      def instantiate_authorizations(authorizations)
+        return [] unless authorizations
+        authorizations.map do |authorization|
+          Authorization.new(authorization)
+        end
+      end
+    end
+  end
+end
