4cb03d437663e4abf0cde627c5eb8d5f73e01038
Vitor Kusiaki <vkusiaki@gmail.com>
2015-06-16 14:49:49 -0300

    Uses collection as a return of authorization find by date

---
+++ spec/pagseguro/authorization/collection_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe PagSeguro::Authorization::Collection do
+  let(:permission) { double(:permission) }
+  let(:authorizations) do
+    [
+      { code: '1234', created_at: Date.today,  permissions: [permission] },
+      { code: '4321', created_at: Date.today,  permissions: [permission, permission] }
+    ]
+  end
+  subject { described_class.new({authorizations: authorizations}) }
+
+  describe "initialization" do
+    context "when options[:errors] is present" do
+      let(:errors) { double(:errors) }
+      subject { described_class.new({errors: errors}) }
+
+      it "sets errors" do
+        expect(subject.errors).to eq(errors)
+      end
+
+      it "has no authorizations" do
+        expect(subject).to be_empty
+      end
+    end
+
+    context "when options[:authorizations] is present" do
+      it { expect(subject).to be_any }
+      it { expect(subject).not_to be_empty }
+
+      it "has authorizations instances" do
+        subject.each do |installment|
+          expect(installment).to be_a(PagSeguro::Authorization)
+        end
+      end
+    end
+  end
+
+  describe "#errors" do
+    it { expect(subject.errors).to be_a(PagSeguro::Errors) }
+    it { expect(subject.errors).to be_empty }
+  end
+
+  describe "method delegation" do
+    it { subject.respond_to? (:each) }
+    it { subject.respond_to? (:empty?) }
+    it { subject.respond_to? (:any?) }
+  end
+end
