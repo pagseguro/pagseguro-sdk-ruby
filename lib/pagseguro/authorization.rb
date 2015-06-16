c23df938107f6aa3813b1abe06218a262754c894
Vitor Kusiaki <vkusiaki@gmail.com>
2015-06-16 14:49:32 -0300

    Refactors authorization service

--- lib/pagseguro/authorization.rb
+++ lib/pagseguro/authorization.rb
@@ -17,14 +17,20 @@ module PagSeguro
 
     # Find an authorization by it's notification code
     def self.find_by_notification_code(code, options = {})
-      request = Request.get("authorizations/notifications/#{code}", options)
+      request = Request.get("authorizations/notifications/#{code}", "v2", options)
       new Response.new(request).serialize
     end
 
     # Find an authorization by it's code
     def self.find_by_code(code, options = {})
-      request = Request.get("authorizations/#{code}", options)
+      request = Request.get("authorizations/#{code}", "v2", options)
       new Response.new(request).serialize
+    end
+
+    def self.find_by_date(options)
+      request = Request.get("authorizations", "v2", RequestSerializer.new(options).to_params)
+      response = Response.new(request).serialize_collection
+      Collection.new(response)
     end
   end
 end
