b7399ac4b35ce2b08335b097e5280fafd4183984
Vitor Kusiaki <vkusiaki@gmail.com>
2015-06-16 13:01:52 -0300

    Updates merge unresolved conflicts

--- spec/pagseguro/installment_spec.rb
+++ spec/pagseguro/installment_spec.rb
@@ -29,7 +29,7 @@ describe PagSeguro::Installment do
         expect(PagSeguro::Installment).to receive(:load_from_response)
           .with(request)
 
-        PagSeguro::Installment.find("100.00", { card_brand: :visa })
+        PagSeguro::Installment.find("100.00", :visa)
       end
     end
 
