e1817a40973d0192320dff62075ed264e5783dfa
Vitor Kusiaki <vkusiaki@gmail.com>
2015-06-17 09:33:38 -0300

    Moves errors responsability to authorization response class

--- spec/pagseguro/authorization/response_spec.rb
+++ spec/pagseguro/authorization/response_spec.rb
@@ -1,24 +1,23 @@
 require "spec_helper"
 
 describe PagSeguro::Authorization::Response do
+  let(:xml) { File.read("./spec/fixtures/authorization/find_authorization.xml") }
   let(:http_response) do
-    response = double(body: "", code: 200, content_type: "text/xml", "[]" => nil)
+    response = double(body: xml, code: 200, content_type: "text/xml", "[]" => nil)
     Aitch::Response.new({xml_parser: Aitch::XMLParser}, response)
   end
 
-  subject { described_class.new(http_response) }
 
   describe "#serialize" do
+    subject { described_class.new(http_response, authorization) }
+    let(:authorization) { PagSeguro::Authorization.new }
+
     context "when request succeeds" do
       let(:serializer) { double(:serializer) }
-      let(:serialized_data) { double(:serialized_data) }
+      let(:serialized_data) { { code: "1234"} }
 
       it "returns a hash with serialized response data" do
-        expect(PagSeguro::Authorization::ResponseSerializer).to receive(:new)
-          .and_return(serializer)
-        expect(serializer).to receive(:serialize).and_return(serialized_data)
-
-        expect(subject.serialize).to eq(serialized_data)
+        expect(subject.serialize).to eq(authorization)
       end
     end
 
@@ -28,7 +27,7 @@ describe PagSeguro::Authorization::Response do
       end
 
       it "returns a hash with an errors object" do
-        expect(subject.serialize[:errors]).to be_a(PagSeguro::Errors)
+        expect(subject.serialize.errors).to be_a(PagSeguro::Errors)
       end
     end
   end
