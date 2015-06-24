ae8ff450069f28d126c228a272b8629861096db8
Dmitry Rocha <dmitryrck@gmail.com>
2015-06-24 14:56:42 -0300

    Fix how to check if a item is duplicated at PagSeguro::Items.

--- lib/pagseguro/items.rb
+++ lib/pagseguro/items.rb
@@ -13,15 +13,19 @@ module PagSeguro
     def <<(item)
       item = ensure_type(Item, item)
 
-      if include?(item)
-        item.quantity += 1
+      original_item = include?(item)
+
+      if original_item
+        original_item.quantity += 1
       else
         @store << item
       end
     end
 
     def include?(item)
-      @store.find {|stored_item| stored_item.id == ensure_type(Item, item).id }
+      @store.detect do |stored_item|
+        stored_item.id == item.id && stored_item.description == item.description
+      end
     end
   end
 end
