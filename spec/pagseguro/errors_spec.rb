4c88cad0ddb65143ca7e00dec59051ce97ae6bd8
Dmitry Rocha <dmitryrck@gmail.com>
2015-10-27 13:28:48 -0300

    Add not found when response from server is not found.

--- spec/pagseguro/errors_spec.rb
+++ spec/pagseguro/errors_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 
 describe PagSeguro::Errors do
   let(:response) { double }
-  let(:http_response) { double(:http_response, unauthorized?: true, bad_request?: false) }
+  let(:http_response) { double(:http_response, unauthorized?: true, bad_request?: false, not_found?: false) }
 
   context "when have no response" do
     it "returns errors" do
@@ -16,12 +16,24 @@ describe PagSeguro::Errors do
     subject(:errors) { PagSeguro::Errors.new(response) }
 
     before do
-      response.stub unauthorized?: true, bad_request?: false
+      response.stub unauthorized?: true, bad_request?: false, not_found?: false
       errors.add(http_response)
     end
 
     it { expect(errors).not_to be_empty }
     it { expect(errors).to include(I18n.t("pagseguro.errors.unauthorized")) }
+  end
+
+  context "when not found" do
+    subject(:errors) { PagSeguro::Errors.new(response) }
+
+    before do
+      response.stub unauthorized?: true, bad_request?: false, not_found?: true
+      errors.add(http_response)
+    end
+
+    it { expect(errors).not_to be_empty }
+    it { expect(errors).to include(I18n.t("pagseguro.errors.not_found")) }
   end
 
   context "when message can't be translated" do
@@ -40,7 +52,7 @@ describe PagSeguro::Errors do
     subject(:errors) { PagSeguro::Errors.new(response) }
 
     before do
-      response.stub data: xml, unauthorized?: false, bad_request?: true
+      response.stub data: xml, unauthorized?: false, bad_request?: true, not_found?: true
     end
 
     it { expect(errors).to include("Sample message") }
@@ -62,7 +74,7 @@ describe PagSeguro::Errors do
     subject(:errors) { PagSeguro::Errors.new(response) }
 
     before do
-      response.stub data: xml, unauthorized?: false, bad_request?: true
+      response.stub data: xml, unauthorized?: false, bad_request?: true, not_found?: false
     end
 
     it { expect(errors).to include("O parâmetro email deve ser informado.") }
@@ -85,7 +97,7 @@ describe PagSeguro::Errors do
     subject(:errors) { PagSeguro::Errors.new(response) }
 
     before do
-      response.stub data: xml, unauthorized?: false, bad_request?: true
+      response.stub data: xml, unauthorized?: false, bad_request?: true, not_found?: true
       errors.add(http_response)
     end
 
