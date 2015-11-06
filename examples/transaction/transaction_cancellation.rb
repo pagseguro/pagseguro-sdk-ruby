0a653f0629b6505b4f8e0ef92c97cd3e7f16e1d1
Dmitry Rocha <dmitryrck@gmail.com>
2015-11-06 13:40:34 -0300

    Update examples/transaction_cancellation.rb.

--- examples/transaction/transaction_cancellation.rb
+++ examples/transaction/transaction_cancellation.rb
@@ -3,11 +3,11 @@ require_relative "boot"
 cancellation = PagSeguro::TransactionCancellation.new
 cancellation.transaction_code = "AFB8FCF29496401681257C1ECE3A98FF"
 
-response = cancellation.register
+cancellation.register
 
-if response.errors.any?
-  puts response.errors.join("\n")
+if cancellation.errors.any?
+  puts cancellation.errors.join("\n")
 else
   puts "=> CANCELLATION RESPONSE"
-  puts response.result
+  puts cancellation.result
 end
