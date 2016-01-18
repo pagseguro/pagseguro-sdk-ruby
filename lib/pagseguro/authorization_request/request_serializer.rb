824e8da023fdcca64c3bb8ddef5f77e1d6a33b8e
Rafael Belo <rafaelrpbelo@gmail.com>
2016-01-18 17:37:04 -0300

    Modify RequestSerializer on AuthorizationRequest
    - Change it to serialize XML data

--- lib/pagseguro/authorization_request/request_serializer.rb
+++ lib/pagseguro/authorization_request/request_serializer.rb
@@ -1,29 +1,141 @@
 module PagSeguro
   class AuthorizationRequest
     class RequestSerializer
-      attr_reader :authorization_request
 
-      def initialize(authorization_request)
-        @authorization_request = authorization_request
+      attr_accessor :request
+
+      def initialize(request)
+        @request = request
+        @builder = Nokogiri::XML::Builder
       end
 
-      def to_params
-        params[:credentials] = authorization_request.credentials if authorization_request.credentials
-        params[:notificationURL] = authorization_request.notification_url
-        params[:redirectURL] = authorization_request.redirect_url
-        params[:permissions] = serialize_permissions(authorization_request.permissions)
-        params[:reference] = authorization_request.reference if authorization_request.reference
-
-        params
+      def build_xml
+        build.to_xml(save_with:
+          Nokogiri::XML::Node::SaveOptions::NO_EMPTY_TAGS |
+          Nokogiri::XML::Node::SaveOptions::FORMAT
+        )
       end
 
       private
-      def params
-        @params ||= {}
+
+      attr_reader :builder
+
+      def build
+        builder.new(encoding: PagSeguro.encoding) do |xml|
+          xml.send(:authorizationRequest) {
+            xml.send(:reference, request.reference) if request.reference
+            xml.send(:redirectURL, request.redirect_url)
+            xml.send(:notificationURL, request.notification_url)
+            serialize_permissions(xml, request.permissions)
+            serialize_account(xml, request.account)
+          }
+        end
       end
 
-      def serialize_permissions(permissions)
-        permissions.map { |value| PagSeguro::AuthorizationRequest::PERMISSIONS[value] }.join(',')
+      def serialize_permissions(xml, permissions)
+        return unless permissions.any?
+        xml.send(:permissions) {
+          format_permissions(permissions).each do |permission|
+            xml.send(:code, permission)
+          end
+        }
+      end
+
+      def format_permissions(permissions)
+        permissions.map { |value| PagSeguro::AuthorizationRequest::PERMISSIONS[value] }
+      end
+
+      def serialize_account(xml, account)
+        return unless account
+
+        xml.send(:account) {
+          xml.send(:email, account.email) if account.email
+          xml.send(:type, account.type) if account.type
+
+          if account.person
+            serialize_person(xml, account.person)
+          elsif account.company
+            serialize_company(xml, account.company)
+          end
+        }
+      end
+
+      def serialize_person(xml, person)
+        return unless person
+
+        xml.send(:person) {
+          xml.send(:name, person.name) if person.name
+          xml.send(:birthDate, person.birth_date.to_s) if person.birth_date
+          serialize_address(xml, person.address)
+          serialize_documents(xml, person.documents)
+          serialize_phones(xml, person.phones)
+        }
+      end
+
+      def serialize_company(xml, company)
+        return unless company
+
+        xml.send(:company) {
+          xml.send(:name, company.name) if company.name
+          xml.send(:displayName, company.display_name) if company.display_name
+          xml.send(:websiteURL, company.website_url) if company.website_url
+          serialize_partner(xml, company.partner)
+          serialize_phones(xml, company.phones)
+          serialize_documents(xml, company.documents)
+          serialize_address(xml, company.address)
+        }
+      end
+
+      def serialize_address(xml, address)
+        return unless address
+
+        xml.send(:address) {
+          xml.send(:postalCode, address.postal_code) if address.postal_code
+          xml.send(:street, address.street) if address.street
+          xml.send(:number, address.number) if address.number
+          xml.send(:complement, address.complement) if address.complement
+          xml.send(:district, address.district) if address.district
+          xml.send(:city, address.city) if address.city
+          xml.send(:state, address.state) if address.state
+          xml.send(:country, address.country) if address.country
+        }
+      end
+
+      def serialize_phones(xml, phones)
+        return unless phones.any?
+
+        xml.send(:phones) {
+          phones.each do |phone|
+            xml.send(:phone) {
+              xml.send(:type, phone.type)
+              xml.send(:areaCode, phone.area_code)
+              xml.send(:number, phone.number)
+            }
+          end
+        }
+      end
+
+      def serialize_documents(xml, documents)
+        return unless documents.any?
+
+        xml.send(:documents) {
+          documents.each do |document|
+            xml.send(:document) {
+              xml.send(:type, document.type)
+              xml.send(:value, document.value)
+            }
+          end
+        }
+      end
+
+      def serialize_partner(xml, partner)
+        return unless partner
+
+        xml.send(:partner) {
+          xml.send(:name, partner.name) if partner.name
+          xml.send(:birthDate, partner.birth_date.to_s) if partner.birth_date
+          serialize_documents(xml, partner.documents)
+        }
       end
     end
   end
--- spec/pagseguro/authorization_request/request_serializer_spec.rb
+++ spec/pagseguro/authorization_request/request_serializer_spec.rb
@@ -1,23 +1,323 @@
-require "spec_helper"
+require 'spec_helper'
+
+shared_examples 'when address is assigned' do
+  it 'can have a postal code' do
+    address[:postal_code] = '01452002'
+
+    postal_code_parsed = node.at_css('> postalCode').text
+    expect(postal_code_parsed).to eq '01452002'
+  end
+
+  it 'can have a street' do
+    address[:street] = 'Av. Brig. Faria Lima'
+
+    street_parsed = node.at_css('> street').text
+    expect(street_parsed).to eq 'Av. Brig. Faria Lima'
+  end
+
+  it 'can have a number' do
+    address[:number] = '1384'
+
+    number_parsed = node.at_css('> number').text
+    expect(number_parsed).to eq '1384'
+  end
+
+  it 'can have a complement' do
+    address[:complement] = '5o andar'
+
+    complement_parsed = node.at_css('> complement').text
+    expect(complement_parsed).to eq '5o andar'
+  end
+
+  it 'can have a district' do
+    address[:district] = 'Jardim Paulistano'
+
+    district_parsed = node.at_css('> district').text
+    expect(district_parsed).to eq 'Jardim Paulistano'
+  end
+
+  it 'can have a city' do
+    address[:city] = 'Sao Paulo'
+
+    city_parsed = node.at_css('> city').text
+    expect(city_parsed).to eq 'Sao Paulo'
+  end
+
+  it 'can have a state' do
+    address[:state] = 'SP'
+
+    state_parsed = node.at_css('> state').text
+    expect(state_parsed).to eq 'SP'
+  end
+
+  it 'can have a country' do
+    address[:country] = 'BRA'
+
+    country_parsed = node.at_css('> country').text
+    expect(country_parsed).to eq 'BRA'
+  end
+end
+
+shared_examples 'when documents is assigned' do
+  context 'when one document is given' do
+    before do
+      documents << { type: 'CPF', value: '99988877766' }
+    end
+
+    let(:documents_parsed) { node.css('> document') }
+
+    it 'must have the correct type' do
+      expect( documents_parsed.at_css('> type').text ).to eq 'CPF'
+    end
+
+    it 'must have the correct value' do
+      expect( documents_parsed.at_css('> value').text ).to eq '99988877766'
+    end
+  end
+
+  context 'when multiple documents is given' do
+    before do
+      documents << { type: 'CPF', value: '99988877766' }
+      documents << { type: 'CNPJ', value: '17302417000101' }
+    end
+
+    let(:documents_parsed) do
+      node.css('> document').map do |node|
+        {
+          type: node.at_css('> type').text,
+          value: node.at_css('> value').text
+        }
+      end
+    end
+
+    it 'must have the correct number of documents' do
+      expect(documents_parsed.size).to eq 2
+    end
+
+    it 'must have the correct documents' do
+      expect(documents_parsed).to contain_exactly(
+        { type: 'CPF', value: '99988877766' },
+        { type: 'CNPJ', value: '17302417000101' }
+      )
+    end
+  end
+end
+
+shared_examples 'when phones is assigned' do
+  context 'when one phone is given' do
+    before do
+      phones << { type: 'HOME', area_code: '11', number: '30302323' }
+    end
+
+    let(:phone_parsed) { node.at_css('> phone') }
+
+    it 'must have a type' do
+      expect( phone_parsed.at_css('> type').text ).to eq 'HOME'
+    end
+
+    it 'must have an area code' do
+      expect( phone_parsed.at_css('> areaCode').text ).to eq '11'
+    end
+
+    it 'must have a number' do
+      expect( phone_parsed.at_css('> number').text ).to eq '30302323'
+    end
+  end
+
+  context 'can have multiple phones' do
+    before do
+      phones << { type: 'HOME', area_code: '11', number: '30302323' }
+      phones << { type: 'MOBILE', area_code: '11', number: '976302323' }
+    end
+
+    let(:phones_parsed) do
+      node.css('> phone').map do |phone|
+        {
+          type: phone.at_css('> type').text,
+          area_code: phone.at_css('> areaCode').text,
+          number: phone.at_css('> number').text
+        }
+      end
+    end
+
+    it 'must have the correct number of phones' do
+      expect(phones_parsed.size).to eq 2
+    end
+
+    it 'must have the correct phones' do
+      expect(phones_parsed).to contain_exactly(
+        { type: 'HOME', area_code: '11', number: '30302323' },
+        { type: 'MOBILE', area_code: '11', number: '976302323' }
+      )
+    end
+  end
+end
 
 describe PagSeguro::AuthorizationRequest::RequestSerializer do
-  let(:credentials) { PagSeguro::ApplicationCredentials.new('app11', 'sada') }
+  let(:credentials) { PagSeguro::ApplicationCredentials.new('app123', 'key123') }
   let(:options) do
     {
       credentials: credentials,
-      permissions: [:checkouts, :notifications],
-      notification_url: 'foo.com',
-      redirect_url: 'bar.com',
-      reference: 'ref1234'
+      permissions: [:checkouts, :searches, :notifications],
+      notification_url: 'http://seusite.com.br/redirect',
+      redirect_url: 'http://seusite.com.br/notification'
     }
   end
-  let(:authorization) { PagSeguro::AuthorizationRequest.new(options) }
-  let(:serializer) { described_class.new(authorization) }
-  let(:params) { serializer.to_params }
 
-  it{ expect(params).to include(credentials: credentials) }
-  it{ expect(params).to include(permissions: 'CREATE_CHECKOUTS,RECEIVE_TRANSACTION_NOTIFICATIONS') }
-  it{ expect(params).to include(notificationURL: 'foo.com') }
-  it{ expect(params).to include(redirectURL: 'bar.com') }
-  it{ expect(params).to include(reference: 'ref1234') }
+  let(:authorization_request) { PagSeguro::AuthorizationRequest.new(options) }
+  let(:serializer) { described_class.new(authorization_request) }
+  let(:xml) { serializer.build_xml }
+  let(:xml_parsed) { Nokogiri::XML(xml) }
+  let(:node) { xml_parsed.at_css('authorizationRequest') }
+
+  it 'encoding should be equal to application encoding' do
+    encoding = xml_parsed.encoding
+    expect(encoding).to eq PagSeguro.encoding
+  end
+
+  it 'must have a authorizationRequest' do
+    expect(node).to be_a_instance_of Nokogiri::XML::Element
+  end
+
+  it 'must have the correct permissions' do
+    permissions = options[:permissions].map{|value| PagSeguro::AuthorizationRequest::PERMISSIONS[value] }
+    permissions_code_parsed = xml_parsed.css('authorizationRequest > permissions > code').map(&:text)
+
+    expect(permissions).to eq permissions_code_parsed
+  end
+
+  it 'must have a redirect URL' do
+    redirect_url_parsed = xml_parsed.at_css('authorizationRequest > redirectURL').text
+    expect(redirect_url_parsed).to eq options[:redirect_url]
+  end
+
+  it 'must have a notification URL' do
+    notification_url = xml_parsed.at_css('authorizationRequest > notificationURL').text
+    expect(notification_url).to eq options[:notification_url]
+  end
+
+  context 'when an account is assigned' do
+    let(:account) { {} }
+    let(:options) { super().merge(account: account) }
+    let(:node) { super().at_css('> account') }
+
+    it 'can have an email' do
+      account[:email] = 'usuario@seusite.com.br'
+      expect( node.at_css('> email').text ).to eq 'usuario@seusite.com.br'
+    end
+
+    it 'can have a type' do
+      account[:type] = 'SELLER'
+      expect( node.at_css('> type').text ).to eq 'SELLER'
+    end
+
+    context 'when a person is assigned'  do
+      let(:person) { {} }
+      let(:account) { super().merge(person: person) }
+      let(:node) { super().at_css('> person') }
+
+      it 'can have name' do
+        person[:name] = 'Antonio Carlos'
+        expect( node.at_css('> name').text ).to eq 'Antonio Carlos'
+      end
+
+      it 'can have birth date' do
+        person[:birth_date] = Date.new(1982, 2, 5)
+        expect( node.at_css('> birthDate').text ).to eq '1982-02-05'
+      end
+
+      context 'when the address is assigned' do
+        let(:address) { {} }
+        let(:person) { super().merge(address: address) }
+        let(:node) { super().at_css('> address') }
+
+        include_examples 'when address is assigned'
+      end
+
+      context 'when the documents is assigned' do
+        let(:documents) { [] }
+        let(:person) { super().merge({ documents: documents }) }
+        let(:node) { super().at_css('> documents') }
+
+        include_examples 'when documents is assigned'
+      end
+
+      context 'when the phones' do
+        let(:phones) { [] }
+        let(:person) { super().merge({ phones: phones }) }
+        let(:node) { super().at_css('> phones') }
+
+        include_examples 'when phones is assigned'
+      end
+    end
+
+    context 'when the company is assigned' do
+      let(:company) { {} }
+      let(:account) { super().merge({ company: company }) }
+      let(:node) { super().at_css('> company') }
+
+      it 'can have a name' do
+        company[:name] = 'Seu Site'
+        expect( node.at_css('> name').text ).to eq 'Seu Site'
+      end
+
+      it 'can have a display name' do
+        company[:display_name] = 'Seu Site'
+        expect( node.at_css('> displayName').text ).to eq 'Seu Site'
+      end
+
+      it 'can have a website url' do
+        company[:website_url] = 'http://www.seusite.com.br'
+        expect( node.at_css('> websiteURL').text ).to eq 'http://www.seusite.com.br'
+      end
+
+      context 'when the address is assigned' do
+        let(:address) { {} }
+        let(:company) { super().merge(address: address) }
+        let(:node) { super().at_css('> address') }
+
+        include_examples 'when address is assigned'
+      end
+
+      context 'when the documents is assigned' do
+        let(:documents) { [] }
+        let(:company) { super().merge({ documents: documents }) }
+        let(:node) { super().at_css('> documents') }
+
+        include_examples 'when documents is assigned'
+      end
+
+      context 'when the phones is assigned' do
+        let(:phones) { [] }
+        let(:company) { super().merge({ phones: phones }) }
+        let(:node) { super().at_css('> phones') }
+
+        include_examples 'when phones is assigned'
+      end
+
+      context 'when the partner is assigned' do
+        let(:partner) { {} }
+        let(:company) { super().merge({ partner: partner }) }
+        let(:node) { super().at_css('> partner') }
+
+        it 'can have a name' do
+          partner[:name] = 'Antonio Carlos'
+          expect( node.at_css('> name').text ).to eq 'Antonio Carlos'
+        end
+
+        it 'can have a birth date' do
+          partner[:birth_date] = Date.new(1982, 2, 5)
+          expect( node.at_css('> birthDate').text ).to eq '1982-02-05'
+        end
+
+        context 'when the documents is assigned' do
+          let(:documents) { [] }
+          let(:partner) { super().merge({ documents: documents }) }
+          let(:node) { super().at_css('> documents') }
+
+          include_examples 'when documents is assigned'
+        end
+      end
+    end
+  end
 end
