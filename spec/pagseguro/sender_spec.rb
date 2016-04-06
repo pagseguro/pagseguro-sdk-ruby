b9ab209a691ca78b62d576f8344760ae46b925f8
Antonio Filho <antonio@compilandosolucao.com.br>
2016-04-06 20:51:45 -0300

    Add cnpj attribute for sender.

--- spec/pagseguro/sender_spec.rb
+++ spec/pagseguro/sender_spec.rb
@@ -4,6 +4,7 @@ describe PagSeguro::Sender do
   it_assigns_attribute :name
   it_assigns_attribute :email
   it_assigns_attribute :cpf
+  it_assigns_attribute :cnpj
   it_assigns_attribute :hash
   it_ensures_type PagSeguro::Phone, :phone
   it_ensures_type PagSeguro::Document, :document
