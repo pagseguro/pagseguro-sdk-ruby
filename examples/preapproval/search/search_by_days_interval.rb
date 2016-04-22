74f2fd914681d38c5de39394e3dc110e4a0baf70
Rafael Belo <rafaelrpbelo@gmail.com>
2016-04-22 14:50:46 -0300

    Add details to search by days interval

--- examples/preapproval/search/search_by_days_interval.rb
+++ examples/preapproval/search/search_by_days_interval.rb
@@ -7,21 +7,39 @@ require_relative '../../boot'
 #
 # P.S: See the boot file example for more details.
 
+email = 'user@example.com'
+token = 'TOKEN'
+interval = 30
+
 credentials = PagSeguro::AccountCredentials.new('user@example.com', 'TOKEN')
 options = {
   credentials: credentials,
-  interval: 30
+  interval: interval
 }
 report = PagSeguro::Subscription.search_by_days_interval(options)
 
-if report.errors.empty?
+if report.valid?
+  puts "====== Report ======"
 
   while report.next_page?
     report.next_page!
 
-    puts report.subscriptions.inspect
-  end
+    puts "=> Page #{report.page}/#{report.total_pages}"
+    report.subscriptions.each_with_index do |subscription, index|
+      puts "Subscription #{index+1}/#{report.results}:"
+      puts "  code: #{subscription.code}"
+      puts "  name: #{subscription.name}"
+      puts "  date: #{subscription.date}"
+      puts "  tracker: #{subscription.tracker}"
+      puts "  status: #{subscription.status}"
+      puts "  last event date: #{subscription.last_event_date}"
+      puts "  charge: #{subscription.charge}"
+      puts "  reference: #{subscription.reference}"
+    end
 
+    puts "- End page "
+  end
+  puts " - End report"
 else
   puts "Errors:"
   puts report.errors.join("\n")
