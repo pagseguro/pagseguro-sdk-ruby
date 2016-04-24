d9c8c1fe08fee4b848327cb3b601353e1df32163
Ravi <cerebroso@gmail.com>
2016-04-24 20:36:42 -0300

    tests whether the configuration values are lost in a threaded environment

--- spec/pagseguro/config_spec.rb
+++ spec/pagseguro/config_spec.rb
@@ -2,4 +2,24 @@ require "spec_helper"
 
 describe PagSeguro::Config do
   it_behaves_like "a configuration"
+
+  it "should preserve values across threads" do
+    PagSeguro.configure do |config|
+      config.token = 'firsttoken'
+      config.email = 'some@email.com'
+    end
+
+    thread = Thread.new do
+      PagSeguro.configure do |config|
+        config.token = 'secondtoken'
+        config.email = 'other@email.com'
+      end
+
+      expect(PagSeguro.token).to eql('secondtoken')
+    end
+
+    thread.join
+
+    expect(PagSeguro.token).to eql('firsttoken')
+  end
 end
